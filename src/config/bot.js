60cb4f7c5676115d5922aa9771ffc6f457dbc9fa
panthertbs <freefiremalayalam2006@gmail.com>
2026-09-21 10:26:12 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "LETS TALK",     // this is what people actually see
+        state: "Server Launching Soon",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
